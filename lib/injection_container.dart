a027ac7e45e364e172b819fbc64696c02d8f0926
Rishabh <immrishabh@gmail.com>
2020-04-21 00:30:43 +0530

    Initial Commit.

---
+++ lib/injection_container.dart
@@ -0,0 +1,17 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// Service Locator Instance
+final sl = GetIt.instance; 
+
+Future<void> init() async {
+  //! Features
+
+  //! Core
+
+  //! External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => DataConnectionChecker());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:teesco/core/util/log_wrapper.dart';
 
 import 'core/res/strings.dart';
+import 'injection_container.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await init();
+
+  Log.i(
+    tag: "HomeApp",
+    message: "Initialization Done!\nRunning Application...",
+  );
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -11,6 +23,14 @@ class MyApp extends StatelessWidget {
       title: Strings.app_name,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(Strings.app_name),
+        ),
+        body: Center(
+          child: Text('Welcome to Teesco Application!'),
+        ),
       ),
     );
   }
