c9a224ae50f63e86f07f741150826428d2dee30b
divy-arpit <61823356+divy-arpit@users.noreply.github.com>
2020-06-02 18:30:34 +0530

    Removed unnecessary variables

--- lib/screens/creat_group/create_group_screen.dart
+++ lib/screens/creat_group/create_group_screen.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import './widgets/checkbox_list_tile.dart';
 
 class CreateGroupScreen extends StatelessWidget {
-  final String name, role;
 
-  CreateGroupScreen(this.name, this.role);
+
+
 
   @override
   Widget build(BuildContext context) {
