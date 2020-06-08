8040344c34ad680bb53f378f467bb419500f87ec
Sahil Silare <32628578+sahil9001@users.noreply.github.com>
2020-06-08 09:50:08 +0530

    feat: Added query screen UI (#57)
    * feat: Added query screen UI
    
    * modified:   lib/main.dart

---
+++ lib/screens/query_section/widgets/query_data.dart
@@ -0,0 +1,30 @@
+class dummyData{
+  List dummy=[
+    {"message_id":1,
+    "message":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    "timestamp":1591354516,
+    "name":"",
+    "isMe":true},
+    {"message_id":2,
+    "message":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut",
+    "timestamp":1591354516,
+    "name":"John",
+    "isMe":false},
+    {"message_id":1,
+    "message":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut",
+    "timestamp":1591354516,
+    "name":"Amy",
+    "isMe":false},
+    {"message_id":1,
+    "message":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut",
+    "timestamp":1591354516,
+    "name":"",
+    "isMe":true},
+    {"message_id":1,
+    "message":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut",
+    "timestamp":1591354516,
+    "name":"Chris",
+    "isMe":false},
+    
+  ];
+}
